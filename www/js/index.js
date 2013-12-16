854d3e7f3c2018e3fada2fb3b8aa1bab3675a8c3
Herm Wong <herm.wong@gmail.com>
2013-12-16 11:24:15 -0800

    [#20] wired up login form and log out button for Phonegap Build integration

--- www/js/index.js
+++ www/js/index.js
@@ -1,2 +1,18 @@
 var fileExplorerPage = require('nw.gui');
-fileExplorerPage.Window.get().show();           
+var win = fileExplorerPage.Window.get();   
+
+win.show();    
+
+win.on("close", function () {
+	console.log("window close handler");
+	
+	/*
+	// only want to call this if the user is logged in to PGB
+	global.pg.remote.logout({}, function(e) {
+		if (e) {
+			console.log(e.message);
+			alert(e.message);
+		}
+	});
+	*/
+});
