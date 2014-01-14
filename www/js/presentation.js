3b3da656d613f7ddd5c33e9f251df08492384fc1
hermwong <herm.wong@gmail.com>
2014-01-13 16:29:06 -0800

    [#23] added persistent storage to remember last opened proj. directory by the user, moved some presentation logic into presentation.js

---
+++ www/js/presentation.js
@@ -0,0 +1,30 @@
+function enableFormButtons() {
+    if (global.projDir != "") {
+        global.jQuery("#createProject").prop("disabled", false);
+        global.jQuery("#openProject").prop("disabled", false);
+        global.jQuery("#serveProject").prop("disabled", false);
+        global.jQuery("#buildProject").prop("disabled", false);
+    }    
+}
+
+function disableFormButtons() {
+    global.jQuery("#createProject").prop("disabled", true);
+    global.jQuery("#openProject").prop("disabled", true);
+    global.jQuery("#serveProject").prop("disabled", true);
+    global.jQuery("#buildProject").prop("disabled", true);    
+}
+
+function setButtonLabels() {
+    global.jQuery("#createProject").val("create a new project");
+    global.jQuery("#openProject").val("open a project");    
+    global.jQuery("#serveProject").val("serve a project");   
+    global.jQuery("#buildProject").val("build a project");   
+    global.jQuery("#submit").val("login");
+    global.jQuery("#logout").val("log out");
+}
+
+function initGUI() {
+    setButtonLabels();
+    disableFormButtons();
+    global.jQuery("#buildHolder").hide();
+}
