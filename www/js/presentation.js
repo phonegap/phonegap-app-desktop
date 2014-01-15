b4ee95e351b4e6ac1b51a799390d80a444def9ae
hermwong <herm.wong@gmail.com>
2014-01-15 12:09:25 -0800

    [#9] add stop server functionality

--- www/js/presentation.js
+++ www/js/presentation.js
@@ -11,13 +11,15 @@ function disableFormButtons() {
     global.jQuery("#createProject").prop("disabled", true);
     global.jQuery("#openProject").prop("disabled", true);
     global.jQuery("#serveProject").prop("disabled", true);
+    global.jQuery("#stopServer").prop("disabled", true);
     global.jQuery("#buildProject").prop("disabled", true);    
 }
 
 function setButtonLabels() {
     global.jQuery("#createProject").val("create a new project");
     global.jQuery("#openProject").val("open a project");    
-    global.jQuery("#serveProject").val("serve a project");   
+    global.jQuery("#serveProject").val("serve a project"); 
+    global.jQuery("#stopServer").val("stop server");   
     global.jQuery("#buildProject").val("build a project");   
     global.jQuery("#submit").val("login");
     global.jQuery("#logout").val("log out");
