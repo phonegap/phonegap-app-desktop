55bed463eefcf5bd65b18672e4363869e70e0a21
hermwong <herm.wong@gmail.com>
2014-01-17 15:27:05 -0800

    [#32] replaced start & stop server buttons with a toggle

--- www/js/presentation.js
+++ www/js/presentation.js
@@ -2,22 +2,19 @@ function enableFormButtons() {
     if (global.projDir != "") {
         global.jQuery("#createProject").prop("disabled", false);
         global.jQuery("#openProject").prop("disabled", false);
-        global.jQuery("#serveProject").prop("disabled", false);
+        global.jQuery("#isServerStarted").prop("disabled", false);
     }    
 }
 
 function disableFormButtons() {
     global.jQuery("#createProject").prop("disabled", true);
-    global.jQuery("#openProject").prop("disabled", true);
-    global.jQuery("#serveProject").prop("disabled", true);
-    global.jQuery("#stopServer").prop("disabled", true);   
+    global.jQuery("#openProject").prop("disabled", true); 
+    global.jQuery("#isServerStarted").prop("disabled", true);  
 }
 
 function setButtonLabels() {
-    global.jQuery("#createProject").html("create a new project");
-    global.jQuery("#openProject").html("open a project");    
-    global.jQuery("#serveProject").html("serve a project"); 
-    global.jQuery("#stopServer").html("stop server"); 
+    global.jQuery("#createProject").html("create project");
+    global.jQuery("#openProject").html("open project");    
     global.jQuery("#settings").html("settings");
 }
 
