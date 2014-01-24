8fa90042d6b1eaa06b1caaddb6dda75b6ad40c21
Herm Wong <herm.wong@gmail.com>
2014-01-24 14:52:39 -0800

    [#37] added example menu item & menu item handler to the menu bar

--- www/js/index.js
+++ www/js/index.js
@@ -3,10 +3,18 @@ var win = gui.Window.get();
 
 var menubar = new gui.Menu({ type: 'menubar' });
 var file = new gui.Menu();
-var help = new gui.Menu();
+
 win.menu = menubar;
-win.menu.insert(new gui.MenuItem({ label: 'File', submenu: file}), 1);
-win.menu.append(new gui.MenuItem({ label: 'Help', submenu: help}));
+win.menu.insert(new gui.MenuItem({ label: 'File', submenu: file }), 1);
+
+
+menubar.items[0].submenu.append(new gui.MenuItem({
+   label: 'New',
+   click: function () {
+      alert("test");
+   }
+}));
+
 
 win.show();    
 
