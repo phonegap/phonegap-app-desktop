120d6e16c620b04ae4646808cd6601a25bb5a4b9
Herm Wong <herm.wong@gmail.com>
2014-01-24 14:39:23 -0800

    [#37] display a custom menu in the OS menu bar

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,12 @@
-var fileExplorerPage = require('nw.gui');
-var win = fileExplorerPage.Window.get();   
+var gui = require('nw.gui');
+var win = gui.Window.get();   
+
+var menubar = new gui.Menu({ type: 'menubar' });
+var file = new gui.Menu();
+var help = new gui.Menu();
+win.menu = menubar;
+win.menu.insert(new gui.MenuItem({ label: 'File', submenu: file}), 1);
+win.menu.append(new gui.MenuItem({ label: 'Help', submenu: help}));
 
 win.show();    
 
