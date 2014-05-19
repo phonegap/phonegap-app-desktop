c8ba5e7a81087823da82f5fcd660ad0224e2f3ba
Herm Wong <herm.wong@gmail.com>
2014-05-19 15:58:33 -0700

    [#95] added active & inactive  states to minus & settings sidebar areas

--- www/js/sidebar-handlers.js
+++ www/js/sidebar-handlers.js
@@ -27,6 +27,8 @@ function hideAddCreateProjectOverlay(evt) {
 
 function displayRemoveProjectOverlay(evt) {
     console.log("removeProjectOverlay - minus click handler");
+    global.jQuery("#minus-holder").css("background-color", "rgb(31,35,38)");
+    global.jQuery("#minus").css("background-color", "rgb(31,35,38)");
     global.jQuery("#minus-icon").attr("src", "img/icons/active/minus-active.svg");
     global.jQuery("#removeProjectOverlay").show();
     global.jQuery("#overlay-bg").show();
@@ -36,12 +38,16 @@ function hideRemoveProjectOverlay(evt) {
     global.jQuery("#minus-icon").attr("src", "img/icons/normal/minus.svg");
     global.jQuery("#removeProjectOverlay").hide();
     global.jQuery("#overlay-bg").hide();
+    global.jQuery("#minus-holder").css("background-color", "rgb(45,48,51)");
+    global.jQuery("#minus").css("background-color", "rgb(45,48,51)");
 }
 
 function displaySettingsOverlay(evt) {
     console.log("settingsOverlay - settings click handler");
     // prepopulate port number from localStorage
     global.jQuery("#portNumber").val(localStorage.portNumber);
+    global.jQuery("#settings-holder").css("background-color", "rgb(31,35,38)");
+    global.jQuery("#settings").css("background-color", "rgb(31,35,38)");
     global.jQuery("#settings-icon").attr("src", "img/icons/active/settings-active.svg");
     global.jQuery("#settingsOverlay").show();
     global.jQuery("#overlay-bg").show();
@@ -51,6 +57,8 @@ function hideSettingsOverlay(evt) {
     global.jQuery("#settings-icon").attr("src", "img/icons/normal/settings.svg");
     global.jQuery("#settingsOverlay").hide();
     global.jQuery("#overlay-bg").hide();
+    global.jQuery("#settings-holder").css("background-color", "rgb(45,48,51)");
+    global.jQuery("#settings").css("background-color", "rgb(45,48,51)");
 }
 
 function toggleLog(evt) {
