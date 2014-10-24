e4e5927fa8ff0675aaf18234028f9307681f7843
hermwong <herm.wong@gmail.com>
2014-10-24 09:59:15 -0700

    [#214] make sure the entry object exists before initiating the drag handlers

--- www/js/drag-n-drop-handlers.js
+++ www/js/drag-n-drop-handlers.js
@@ -11,15 +11,17 @@ function handleDrop(evt) {
     var length = evt.originalEvent.dataTransfer.items.length;
     for (var i = 0; i < length; i++) {
         var entry = evt.originalEvent.dataTransfer.items[i].webkitGetAsEntry();
-        if (entry.isFile) {
-            console.log("file");
-            // if user drags a file, put them into the normal add / open project workflow
-            displayAddCreateProjectOverlay();
-        } else if (entry.isDirectory) {
-            console.log("folder: " + entry.fullPath);
-            // prompt user into normal add / open project workflow - we may want to consider automatically adding the folder as a project but we would
-            // need to add logic to determine if the folder contains a valid project. (validProject) ? openExisting : createNew
-            displayAddCreateProjectOverlay();
+        if (entry) {
+            if (entry.isFile) {
+                console.log("file");
+                // if user drags a file, put them into the normal add / open project workflow
+                displayAddCreateProjectOverlay();
+            } else if (entry.isDirectory) {
+                console.log("folder: " + entry.fullPath);
+                // prompt user into normal add / open project workflow - we may want to consider automatically adding the folder as a project but we would
+                // need to add logic to determine if the folder contains a valid project. (validProject) ? openExisting : createNew
+                displayAddCreateProjectOverlay();
+            }            
         }
     }
 }
