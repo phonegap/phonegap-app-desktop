9408671746c45f74c51dee50af995d9302a17dde
hermwong <herm.wong@gmail.com>
2015-01-22 16:23:48 -0800

    [#281] updated the Mac Packager script to include the EULA property
    Former-commit-id: a1248de727c55fad1361d78e075c28f6b8211f0b

--- res/installers/osx/package-pgd.sh
+++ res/installers/osx/package-pgd.sh
@@ -1,3 +1,13 @@
-#! /bin/bash
-test -f ../../../installers/osx/PhoneGapDesktop.dmg && rm ../../../installers/osx/PhoneGapDesktop.dmg
-./create-dmg --window-size 500 300 --icon-size 48 --volname "PhoneGap Desktop Installer" --app-drop-link 330 205 --icon "PhoneGap Desktop" 110 205 ../../../installers/osx/PhoneGapDesktop.dmg ../../../build/PhoneGap/OSX
+#! /bin/bash 
+test -f ../../../installers/osx/PhoneGapDesktop.dmg && rm ../../../installers/osx/PhoneGapDesktop.dmg 
+
+./create-dmg \
+--volname "PhoneGap Desktop Installer" \
+--window-size 500 300 \
+--icon-size 48 \
+--icon PhoneGap.app 100 110 \
+--hide-extension PhoneGap.app \
+--app-drop-link 230 110 \
+--eula license.txt \
+../../../installers/osx/PhoneGapDesktop.dmg \
+../../../build/PhoneGap/OSX
