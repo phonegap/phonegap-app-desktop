e55ddc13415d150957101587d391ed534a0624ca
hermwong <herm.wong@gmail.com>
2015-01-30 17:08:21 -0800

    [#63] updated icon size, position & window size of the Mac DMG so that they would be properly positioned with the new background image
    Former-commit-id: 10f372c1b2b14e9a976a1adbdfe58e61eb9a7622

--- res/installers/osx/package-pgd.sh
+++ res/installers/osx/package-pgd.sh
@@ -3,12 +3,12 @@ test -f ../../../installers/osx/PhoneGapDesktop.dmg && rm ../../../installers/os
 
 ./create-dmg \
 --volname "PhoneGap Desktop Installer" \
---background "bg-500x300.png" \
---window-size 500 300 \
---icon-size 48 \
---icon PhoneGap.app 100 110 \
+--background "bg-512-320.png" \
+--window-size 512 320 \
+--icon-size 64 \
+--icon PhoneGap.app 125 160 \
 --hide-extension PhoneGap.app \
---app-drop-link 230 110 \
+--app-drop-link 385 160 \
 --eula license.txt \
 ../../../installers/osx/PhoneGapDesktop.dmg \
 ../../../build/PhoneGap/OSX
