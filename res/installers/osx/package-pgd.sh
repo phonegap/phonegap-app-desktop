9082bdfa022b4eeb4b65510ee451a2dbd78be1d0
Herm Wong <herm.wong@gmail.com>
2015-02-05 16:25:47 -0800

    [#67] increase the icon sizes in the Mac DMG to match the icon sizes in the mock up

--- res/installers/osx/package-pgd.sh
+++ res/installers/osx/package-pgd.sh
@@ -5,10 +5,10 @@ test -f ../../../installers/osx/PhoneGapDesktop.dmg && rm ../../../installers/os
 --volname "PhoneGap Desktop Installer" \
 --background "bg-512-320.png" \
 --window-size 512 320 \
---icon-size 64 \
---icon PhoneGap.app 125 160 \
+--icon-size 120 \
+--icon PhoneGap.app 128 160 \
 --hide-extension PhoneGap.app \
---app-drop-link 385 160 \
+--app-drop-link 388 160 \
 --eula license.txt \
 ../../../installers/osx/PhoneGapDesktop.dmg \
 ../../../build/PhoneGap/OSX
