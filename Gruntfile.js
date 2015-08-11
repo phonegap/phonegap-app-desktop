9f6666d5d4af58befeb3cefaf4f72c2b95c48202
hermwong <herm.wong@gmail.com>
2015-08-11 16:06:07 -0700

    [#381] removed existing nwjs project and replaced with electron template project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,113 +1,61 @@
 module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    less: {
-      development: {
-        files: {
-          'www/css/index.css': 'src/less/index.less'
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        'build-electron-app': {
+            options: {
+                platforms: ["darwin", "win32"],
+                app_dir: './www',
+                build_dir: './build'
+            }
         }
-      }
-    },
-    nwjs: {
-      options: {
-        version: '0.12.0',
-        build_dir: './build', // Destination for built apps.
-        mac: true,            // OS X support.
-        win: true,            // Windows support.
-        linux32: false,       // Linux 32-bit support.
-        linux64: false,        // Linux 64-bit support.
-        credits: 'www/credits.html',
-        mac_icns: 'www/img/app-icons/icon.icns',
-        winIco: 'www/img/app-icons/icon.ico'
-      },
-      src: ['./www/**/*', './node_modules/phonegap/**/*']
-    },
-    watch: {
-      files: ['./src/less/**/*'],
-      tasks: ['less']
-    }
-  });
+    });
 
-  // Load the grunt plugins.
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-nw-builder');
+    // Load the grunt plugins.
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-electron-app-builder');
 
-  // Register the task to install nodewebkit dependencies.
-  grunt.task.registerTask('install-dependencies', function() {
-    var exec = require('child_process').exec,
+    // Register the task to install dependencies.
+    grunt.task.registerTask('install-dependencies', function() {
+        var exec = require('child_process').exec,
         callback = this.async();
 
-    exec('npm install --production', { cwd: './www' }, function(e, stdout, stderr) {
-      console.log(stdout);
-      callback();
+        exec('npm install --production', { cwd: './www' }, function(e, stdout, stderr) {
+            console.log(stdout);
+            callback();
+        });
     });
-  });
 
-  grunt.task.registerTask('copy-dev-config', function() {
-      //var config = grunt.file.read('./src/config/package.json');
-      grunt.file.copy('./src/config/package.json', './www/package.json');
+    // Remove build directories
+    grunt.task.registerTask('clean-build-dir', function() {
+        var shell = require('shelljs');
+        shell.rm('-rf', './build/darwin');
+        shell.rm('-rf', './build/win32');
+    });
 
-  });
+    // Rename app from Electron to PhoneGap
+    grunt.task.registerTask('rename-app', function() {
+        var fs = require('fs');
+        fs.rename('./build/darwin/Electron.app', './build/darwin/PhoneGap.app');
+        fs.rename('./build/win32/electron.exe', './build/win32/PhoneGap.exe');
+    });
 
-  grunt.task.registerTask('copy-release-config', function() {
-      var config = grunt.file.read('./src/config/package.json');
-      var releaseConfig = config.replace("\"toolbar\": true", "\"toolbar\": false");
-      grunt.file.write('./www/package.json', releaseConfig);
-  });
+    // Open the built app
+    grunt.task.registerTask('open', function() {
+        var os = require('os'),
+            fs = require('fs'),
+            opener = require('opener'),
+            appName = JSON.parse(fs.readFileSync('./www/package.json')).name,
+            macPath = 'build/darwin/appName.app',
+            winPath = 'build/win32/appName.exe';
 
-  grunt.task.registerTask('copy-eula', function() {
-      grunt.file.copy('./src/license.txt', './res/installers/osx/license.txt');
-      grunt.file.copy('./src/license.txt', './res/installers/win/license.txt');
-  });
+            macPath = macPath.replace(/appName/g, appName);
+            winPath = winPath.replace(/appName/g, appName);
 
-  // Remove build directories
-  grunt.task.registerTask('clean-build-dir', function() {
-      var shell = require('shelljs');
-      shell.rm('-rf', './build/PhoneGap/osx32');
-      shell.rm('-rf', './build/PhoneGap/osx64');
-      shell.rm('-rf', './build/PhoneGap/win32');
-      shell.rm('-rf', './build/PhoneGap/win64');
-      shell.rm('-rf', './build/PhoneGap/linux32');
-      shell.rm('-rf', './build/PhoneGap/linux64');
-  });
+            opener((os.platform() === 'darwin') ? macPath : winPath);
+    });
 
-  // Register the task to open an app.
-  grunt.task.registerTask('open', 'Open the app', function() {
-	var fs = require('fs'),
-        os = require('os'),
-        opener = require('opener'),
-        appName = JSON.parse(fs.readFileSync('./www/package.json')).name,
-        architecture = os.arch(),
-        macPath = '',
-        winPath = '';
-
-        switch (architecture) {
-            case 'x64':
-                macPath = 'build/appName/osx64/appName.app',
-                winPath = 'build/appName/win64/appName.exe';
-                break;
-            case 'ia32':
-                macPath = 'build/appName/osx32/appName.app',
-                winPath = 'build/appName/win32/appName.exe';
-                break;
-            default:
-                macPath = 'build/appName/osx/appName.app',
-                winPath = 'build/appName/win/appName.exe';
-
-        }
-
-    macPath = macPath.replace(/appName/g, appName);
-    winPath = winPath.replace(/appName/g, appName);
-
-    opener((os.platform() === 'darwin') ? macPath : winPath);
-  });
-
-  // Default tasks.
-  grunt.registerTask('default', ['install-dependencies', 'less', 'copy-dev-config', 'copy-eula', 'clean-build-dir', 'nwjs', 'open']);
-  grunt.registerTask('release', ['install-dependencies', 'less', 'copy-release-config', 'copy-eula', 'clean-build-dir', 'nwjs']);
+    grunt.registerTask('default', ['install-dependencies', 'clean-build-dir', 'build-electron-app', 'rename-app', 'open']);
 
 };
