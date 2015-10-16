e537215ff469b12f36002973b200fd35a19304e6
Herm Wong <herm.wong@gmail.com>
2015-10-16 13:48:58 -0700

    [#435] updated grunt to use grunt-electron to package the pg-desktop; which provides more options for customizing the electron app. Added custom icon support.
    Former-commit-id: d491b88ae8f917606614b27b91905a1de98f4443

--- www/index.js
+++ www/index.js
@@ -21,7 +21,7 @@ app.on('window-all-closed', function() {
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 450, height: 622, resizable: false, title: 'PhoneGap Desktop', center: true});
+    mainWindow = new BrowserWindow({width: 450, height: 622, resizable: false, title: 'PhoneGap', center: true});
 
     // and load the index.html of the app.
     mainWindow.loadUrl('file://' + __dirname + '/index.html');
