573d2cb6031c15e41dcb3e52e56d5029cdb7fb52
Herm <herm.wong@gmail.com>
2015-12-03 17:04:11 -0800

    [#494] stubbed in rough version of the multi-ip interface
    Former-commit-id: 95f201ded95d189acb8c3178cb676cc1d49b3798

--- www/js/server-status.js
+++ www/js/server-status.js
@@ -18,9 +18,21 @@ function toggleServerStatus(projDir) {
 
             global.pgServer.listen({ port: localStorage.portNumber })
             .on("complete", function(data) {
+
+                if (getDebugFlag()) {
+                    data.addresses = ['192.168.0.1', '10.0.1.4', '172.168.0.1', '127.0.0.1', '192.168.1.100'];
+                }
+
+                var ipAddressesFound = data.addresses.length;
+
                 global.server = data.server;
                 global.isServerRunning = true;
-                serverOnlineState(data);
+
+                if (ipAddressesFound > 1) {
+                    multipleServersOnlineState(data);
+                } else {
+                    serverOnlineState(data, "Server is running on http://" + data.addresses[0] + ":" + data.port);
+                }
 
                 widgetServerOnlineState(global.activeWidget.projectId);
 
@@ -57,9 +69,30 @@ function serverOfflineState() {
     $("#status-field").show();
 }
 
-function serverOnlineState(data) {
+function serverOnlineState(data, label) {
     $("#status-field").show();
     $("#status-field").css("background-color", "rgb(43,169,77)");
-    $("#server-status-label").text("Server is running on http://" + data.address + ":" + data.port);
+    $("#server-status-label").text(label);
     $("#settings-ip").text(data.address + ":");
+}
+
+function multipleServersOnlineState(data) {
+    // data.addresses to access all IP addresses found
+    var ipAddressesFound = data.addresses.length;
+    var ipAddresses = "";
+
+    for (var address of data.addresses) {
+        ipAddresses += "http://";
+        ipAddresses += address;
+        ipAddresses += ":";
+        ipAddresses += data.port;
+        ipAddresses += "<br/>";
+    }
+
+    // set the server status bar
+    serverOnlineState(data, "Server is running on multiple IP addresses:");
+
+    $("#ip-list").html(ipAddresses);
+    $("#ip-list").show();
+
 }
