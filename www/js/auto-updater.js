d863d2eaef3ad9c3cfe5d9213f4f16002a845ede
Herm <herm.wong@gmail.com>
2016-01-13 17:08:15 -0800

    [#382] moved auto-updater code, created server-side script for update checking
    Former-commit-id: 16c715e7cc0bcd1a82299dd026290dcd41bf524b

---
+++ www/js/auto-updater.js
@@ -0,0 +1,24 @@
+function checkForUpdates(updater) {
+
+    var app = remote.require('app');
+
+    updater.on('error', function(e) {
+        console.log('error: ' + e.message);
+    })
+    .on('checking-for-update', function(e) {
+        console.log("checking-for-update");
+    })
+    .on('update-available', function(e) {
+        console.log("update-available");
+    })
+    .on('update-not-available', function(e) {
+        console.log("update-not-available");
+    });
+
+    // TODO: remove hardcoded version before we go into production
+    var feedUrl = 'http://localhost:8080?version=' + '0.2.0';//app.getVersion();
+    console.log(feedUrl);
+
+    updater.setFeedUrl(feedUrl);
+    updater.checkForUpdates();
+}
