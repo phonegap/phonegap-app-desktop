7ab5ecbe3bd82d298bee1367dac412924a60126a
Herm <herm.wong@gmail.com>
2016-01-19 14:16:51 -0800

    [#382] implemented the update notification

--- www/js/auto-updater.js
+++ www/js/auto-updater.js
@@ -3,16 +3,22 @@ function checkForUpdates(updater) {
     var app = remote.require('app');
 
     updater.on('error', function(err, msg) {
+        $('#updateOverlay').hide();
         console.log(msg);
+        displayErrorMessage('PhoneDesktop could not update because of the following error:\n\n' + msg);
     })
     .on('checking-for-update', function(err, msg) {
         console.log("checking-for-update");
     })
     .on('update-available', function(err) {
         console.log("update-available");
+        $('#updateOverlayTitle').text('Update Available');
+        $('#updateOverlayPrompt').text('A newer version of PhoneGap Desktop was found. PhoneGap Desktop will automatically download the update & restart.');
+        $('#updateOverlay').show();
     })
     .on('update-downloaded', function(err) {
         console.log('update-downloaded');
+        $('#updateOverlay').hide();
         updater.quitAndInstall();
     })
     .on('update-not-available', function(err) {
