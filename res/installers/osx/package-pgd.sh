c2e55ab936df915043d71092eb9e99aed5a1312e
Herm <herm.wong@gmail.com>
2016-01-29 11:13:03 -0700

    [#522] removed test update.json file, added compress task and osx-installer tasks to grunt. updated osx dmg script to use correct paths when run from grunt
    Former-commit-id: c18a84141f260b6887b29a7772489d7258c1899b

--- res/installers/osx/package-pgd.sh
+++ res/installers/osx/package-pgd.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
-if test -e ../../../installers/osx64; then
+if test -e ./installers/osx64; then
     echo "osx64 directory already exists"
 else
     echo "creating osx64 directory"
-    mkdir ../../../installers/osx64
+    mkdir ./installers/osx64
 fi
 
-test -f ../../../installers/osx64/PhoneGapDesktop.dmg && rm ../../../installers/osx64/PhoneGapDesktop.dmg
+test -f ./installers/osx64/PhoneGapDesktop.dmg && rm ./installers/osx64/PhoneGapDesktop.dmg
 
-./create-dmg \
+./res/installers/osx/create-dmg \
 --volname "PhoneGap Desktop Installer" \
---background "bg-512-320.png" \
+--background ./res/installers/osx/bg-512-320.png \
 --window-size 512 320 \
 --icon-size 120 \
 --icon PhoneGap.app 128 160 \
 --hide-extension PhoneGap.app \
 --app-drop-link 388 160 \
---eula license.txt \
-../../../installers/osx64/PhoneGapDesktop.dmg \
-../../../build/PhoneGap-darwin-x64/PhoneGap.app
+--eula ./res/installers/osx/license.txt \
+./installers/osx64/PhoneGapDesktop.dmg \
+./build/PhoneGap-darwin-x64/PhoneGap.app
