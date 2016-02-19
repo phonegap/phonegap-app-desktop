ea52411efc15650222d6eab101d02da153d3a28c
Herm <herm.wong@gmail.com>
2016-02-19 15:55:55 -0800

    [#541] added url slug for branch, so that we can test based on branch name

--- update-server/server.js
+++ update-server/server.js
@@ -25,7 +25,6 @@ app.get('/desktop', function (req, res) {
     request(options, function(error, response, body) {
         if(!error && response.statusCode === 200) {
             var serverVersion = body.version;
-            //serverVersion = '0.2.2';
             console.log('serverVersion: ' + serverVersion + ' clientVersion: ' + clientVersion);
             console.log('platform: ' + platform);
 
@@ -44,10 +43,67 @@ app.get('/desktop', function (req, res) {
 
                 if (downloadUrl) {
                     updateJSON = JSON.stringify({url: downloadUrl});
-                    // test update zip (OSX)
-                    //updateJSON = JSON.stringify({url: 'https://github.com/phonegap/phonegap-app-desktop/blob/0.2.2/update-server/test/PhoneGap-test-0.2.2-mac.zip?raw=true'});
-                    // test update zip (Win32)
-                    //updateJSON = JSON.stringify({url: 'https://github.com/phonegap/phonegap-app-desktop/blob/0.2.2/update-server/test/PhoneGap-test-0.2.2-win.zip?raw=true'});
+                    console.log('updateJSON: ' + updateJSON);
+
+                    res.writeHead(200, {'Accept':'application/zip'});
+                    res.statusMessage = 'Update Available';
+                    res.end(updateJSON);
+                } else {
+                    console.log('no downloadUrl - return no update');
+                    res.statusCode = 204;
+                    res.statusMessage = 'No Content';
+                    res.end();
+                }
+
+            } else {
+                console.log('no udpate');
+                res.statusCode = 204;
+                res.statusMessage = 'No Content';
+                res.end();
+            }
+        }
+    });
+});
+
+app.get('/desktop/:branch', function (req, res) {
+    var queryData = req.query;
+    var clientVersion = queryData.version;
+    var platform = queryData.platform;
+    var branch = req.params.branch;
+    var jsonUrl = 'https://raw.githubusercontent.com/phonegap/phonegap-app-desktop/' + branch + '/package.json';
+    var request = require('request');
+
+    console.log('branch url: ' + jsonUrl);
+
+    var options = {
+        url: jsonUrl,
+        json: true
+    };
+
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log(fullUrl);
+
+    request(options, function(error, response, body) {
+        if(!error && response.statusCode === 200) {
+            var serverVersion = body.version;
+            console.log('serverVersion: ' + serverVersion + ' clientVersion: ' + clientVersion);
+            console.log('platform: ' + platform);
+
+            if (serverVersion > clientVersion) {
+                console.log('update available');
+
+                var downloadUrl = null;
+                var updateJSON = null;
+
+                if (platform === 'darwin') {
+                    downloadUrl = body.packages.mac.url;
+                }
+                if (platform === 'win32') {
+                    downloadUrl = body.packages.win.url;
+                }
+
+                if (downloadUrl) {
+                    updateJSON = JSON.stringify({url: downloadUrl});
                     console.log('updateJSON: ' + updateJSON);
 
                     res.writeHead(200, {'Accept':'application/zip'});
