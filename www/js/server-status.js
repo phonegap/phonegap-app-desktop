f4a8d60307bef9f3180864b79930b8c9ba780203
Suraj Pindoria <suraj.pindoria@yahoo.com>
2016-02-23 15:15:34 -0800

    Adding in support for the browser
    Former-commit-id: 0a2a2ece2fdc9522969dc7bce3777b4515fce44c

--- www/js/server-status.js
+++ www/js/server-status.js
@@ -23,7 +23,7 @@ function setServerOnline(projDir) {
             // need to change this for browser platform because of Cordova Issue: CB-5687
             process.env.PWD = projDir;
 
-            global.pgServer.listen({ browser: true, isDesktop: true, phonegap: require('phonegap'), port: localStorage.portNumber }, function(e, data) {
+            global.phonegap.serve({ browser: true, isDesktop: true, phonegap: require('phonegap'), port: localStorage.portNumber }, function(e, data) {
                 var ipAddressesFound = data.addresses.length;
 
                 trackNumIPsFound(ipAddressesFound);
