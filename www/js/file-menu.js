225f07599307944cc807700a9a99eba71315565b
Herm <herm.wong@gmail.com>
2016-02-26 13:42:43 -0800

    [#310] added Copy and Select All clipboard functionality to the PG Desktop

--- www/js/file-menu.js
+++ www/js/file-menu.js
@@ -47,6 +47,21 @@ function getMenuTemplate() {
             ]
         },
         {
+            label: 'Edit',
+            submenu: [
+                {
+                    label: 'Copy',
+                    accelerator: 'CmdOrCtrl+C',
+                    selector: 'copy:'
+                },
+                {
+                    label: 'Select All',
+                    accelerator: 'CmdOrCtrl+A',
+                    selector: 'selectAll:'
+                }
+            ]
+        },
+        {
             label: 'Help',
             submenu: [
                 {
