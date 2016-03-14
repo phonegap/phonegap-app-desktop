82192935a2fa9327434cf6da409358472625f2fc
Herm <herm.wong@gmail.com>
2016-03-14 15:14:27 -0700

    [#569] changed references to new project overlay to project details overlay
    Former-commit-id: 9418716483ae80efdb70b4bd0f1d806b7b22e05b

--- www/js/project-creation-form-validation.js
+++ www/js/project-creation-form-validation.js
@@ -1,23 +1,23 @@
 function displayPhoneGapProjectInFolderError() {
-    $("#newProjectOverlay").addClass("new-project-overlay-project-path-error");
+    $("#projectDetailsOverlay").addClass("project-details-overlay-project-path-error");
     $("#project-path-error-message").text("The chosen folder already contains a PhoneGap project. Please select a different folder.");
     displayProjectPathError();
 }
 
 function displayProjectPathError() {
-    $("#new-project-path-label").addClass("required");
+    $("#project-details-path-label").addClass("required");
     $("#project-path-warning-icon").show();
     $("#project-path-error-message").show();
 }
 
 function hideProjectPathError() {
-    $("#new-project-path-label").removeClass("required");
+    $("#project-details-path-label").removeClass("required");
     $("#project-path-warning-icon").hide();
     $("#project-path-error-message").hide();
 }
 
 function displayProjectNameError() {
-    $("#new-project-name-label").addClass("required");
+    $("#project-details-name-label").addClass("required");
     $("#projectName").addClass("required-input project-creation-warning-icon");
     $("#project-name-error-message").text("You must give your new project a name.");
     $("#project-name-warning").show();
@@ -25,14 +25,14 @@ function displayProjectNameError() {
 }
 
 function hideProjectNameError() {
-    $("#new-project-name-label").removeClass("required");
+    $("#project-details-name-label").removeClass("required");
     $("#projectName").removeClass("required-input project-creation-warning-icon");
     $("#project-name-warning").hide();
     $("#project-name-error-message").hide();
 }
 
 function displayDuplicateProjectNameError() {
-    $("#new-project-name-label").addClass("required");
+    $("#project-details-name-label").addClass("required");
     $("#projectName").addClass("required-input project-creation-warning-icon");
     $("#project-name-error-message").text("A folder with this name already exists in the selected local path. Please choose a new name or a new local path.");
     $("#project-name-warning").show();
@@ -44,14 +44,14 @@ function hideDuplicateProjectNameError() {
 }
 
 function displayProjectIdError() {
-    $("#new-project-id-label").addClass("required");
+    $("#project-details-id-label").addClass("required");
     $("#project-id").addClass("required-input project-creation-warning-icon");
     $("#project-id-warning").show();
     $("#project-id-error-message").show();
 }
 
 function hideProjectIdError() {
-    $("#new-project-id-label").removeClass("required");
+    $("#project-details-id-label").removeClass("required");
     $("#project-id").removeClass("required-input project-creation-warning-icon");
     $("#project-id-warning").hide();
     $("#project-id-error-message").hide();
@@ -60,32 +60,32 @@ function hideProjectIdError() {
 function adjustProjectCreationFormHeight(isProjectPathEmpty, isProjectNameEmpty) {
     if (isProjectPathEmpty && isProjectNameEmpty) {
         // change project creation dialog height to accommodate for project path & project name
-        $("#newProjectOverlay").addClass("new-project-overlay-project-path-and-other-error");
+        $("#projectDetailsOverlay").addClass("project-details-overlay-project-path-and-other-error");
     } else {
         if (isProjectPathEmpty) {
             if (!isProjectNameEmpty) {
                 // change project creation dialog height to accommodate for project path error only
-                $("#newProjectOverlay").addClass("new-project-overlay-project-path-error");
+                $("#projectDetailsOverlay").addClass("project-details-overlay-project-path-error");
             } else {
                 // change project creation dialog height to accommodate for project path error & project name error
-                $("#newProjectOverlay").addClass("new-project-overlay-project-path-and-other-error");
+                $("#projectDetailsOverlay").addClass("project-details-overlay-project-path-and-other-error");
             }
         } else {
             if (isProjectNameEmpty) {
                 // change project creation dialog height to accommodate for project path error & project name error
-                $("#newProjectOverlay").addClass("new-project-overlay-project-name-or-project-id-error");
+                $("#projectDetailsOverlay").addClass("project-details-overlay-project-name-or-project-id-error");
             }
         }
     }
 }
 
 function resetProjectCreationFormHeight() {
-    $("#newProjectOverlay").removeClass("new-project-overlay-all-errors");
-    $("#newProjectOverlay").removeClass("new-project-overlay-project-path-error");
-    $("#newProjectOverlay").removeClass("new-project-overlay-project-path-and-other-error");
-    $("#newProjectOverlay").removeClass("new-project-overlay-project-name-and-project-id-error");
-    $("#newProjectOverlay").removeClass("new-project-overlay-project-name-or-project-id-error");
-    $("#newProjectOverlay").removeClass("new-project-overlay-duplicate-project-name-error");
+    $("#projectDetailsOverlay").removeClass("project-details-overlay-all-errors");
+    $("#projectDetailsOverlay").removeClass("project-details-overlay-project-path-error");
+    $("#projectDetailsOverlay").removeClass("project-details-overlay-project-path-and-other-error");
+    $("#projectDetailsOverlay").removeClass("project-details-overlay-project-name-and-project-id-error");
+    $("#projectDetailsOverlay").removeClass("project-details-overlay-project-name-or-project-id-error");
+    $("#projectDetailsOverlay").removeClass("project-details-overlay-duplicate-project-name-error");
 }
 
 function resetProjectCreationForm() {
