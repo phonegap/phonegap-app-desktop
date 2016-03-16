7534958960d7fb2306ad7443894e38a7cd6fe690
Herm <herm.wong@gmail.com>
2016-03-16 13:14:21 -0700

    [#569] added loading & displaying of templates. Currently have the Snotify template manually added instead of parsed from PhoneGap CLI's package.json
    Former-commit-id: 06191e4eb13a5b526cfce173b8f7a3491a8fadcf

---
+++ www/js/templates.js
@@ -0,0 +1,37 @@
+function getTemplates() {
+    var templates = require("./node_modules/phonegap/package.json").templates;
+    addTemplates(templates);
+}
+
+function templateWidget(name, description) {
+    var str = "";
+    str += "<div class='template-widget'>";
+    if (name === 'hello-world') {
+        str += "<div class='template-list-item'><input type='radio' name='selectedTemplate' value='" + name + "' checked> " + name + "</div>";
+    } else {
+        str += "<div class='template-list-item'><input type='radio' name='selectedTemplate' value='" + name + "'> " + name + "</div>";
+    }
+
+    str += "<div class='template-list-item overlay-form-item-description'>" + description + "</div>";
+    str +="</div>";
+    return str;
+}
+
+function addTemplates(templates) {
+
+    $.each(templates, function(key, value) {
+        console.log(key);
+        console.log(value.description);
+        var templateDOM = templateWidget(key, value.description);
+        $("#templateList").append(templateDOM);
+    });
+
+    var snotify = templateWidget("Snotify", "A full example app with words to add to the description.");
+    $("#templateList").append(snotify);
+
+    $("input[name='selectedTemplate']").change(function() {
+        // Do something interesting here
+        alert(this.value);
+    });
+
+}
