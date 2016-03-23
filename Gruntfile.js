dad353c88c9c3369f753ecc7f78efabf4c4c5f52
Herm <herm.wong@gmail.com>
2016-03-23 15:52:31 -0700

    [#543] disable the ASAR encryption used for compiled Electron binaries

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
                     platform: 'darwin',
                     arch: 'x64',
                     icon: './www/img/app-icons/icon.icns',
-                    asar: true
+                    asar: false
                 }
             },
             winBuild: {
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
                     platform: 'win32',
                     arch: 'ia32',
                     icon: './www/img/app-icons/icon.ico',
-                    asar: true
+                    asar: false
                 }
             }
         },
