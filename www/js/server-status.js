99a630f665af3eefbe7985fa0c00a7d3381846b1
Suraj Pindoria <suraj.pindoria@yahoo.com>
2016-03-29 15:59:58 -0700

    [#574] - update process PWD when we serve new project
    Former-commit-id: 4b84aad0853d39a92ccf55f2fa9f016f15898dec

--- www/js/server-status.js
+++ www/js/server-status.js
@@ -20,6 +20,9 @@ function setServerOnline(projDir) {
         if (exists) {
             process.chdir(projDir);
 
+            // need to change this for browser platform because of Cordova Issue: CB-5687
+            process.env.PWD = projDir;
+
             global.phonegap.serve({ browser: true, isDesktop: true, phonegap: require('phonegap'), port: localStorage.portNumber }, function(e, data) {
                 var ipAddressesFound = data.addresses.length;
 
