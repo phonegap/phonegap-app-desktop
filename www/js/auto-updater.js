586a15e0ee0feb946d37485b10bf5926073f4328
Herm <herm.wong@gmail.com>
2016-06-14 15:13:58 -0700

    [#605] removed references to electron-updater module, stubbed in the workflow to manual update check using the existing electron auto updater api
    Former-commit-id: 56a90280d73d8637025b82af09de8144fe600254

--- www/js/auto-updater.js
+++ www/js/auto-updater.js
@@ -1,7 +1,34 @@
 function checkForUpdates(updater) {
 
     var app = require('electron').remote.app;
+    var jsonUrl = 'https://raw.githubusercontent.com/phonegap/phonegap-app-desktop/master/package.json';
 
+    $.getJSON(jsonUrl).done(function(json) {
+        var clientVersion = app.getVersion();
+        var serverVersion = JSON.stringify(json.version);
+
+        console.log("server: " + serverVersion + " client: " + clientVersion);
+
+        // TODO: change to > before production
+        if (serverVersion < clientVersion) {
+            console.log("update-available");
+            $('#updateOverlayTitle').text('Update Available');
+            $('#updateOverlayPrompt').text('A newer version of PhoneGap Desktop was found. Would you like PhoneGap Desktop to download the update & restart?');
+            $("#overlay-bg").show();
+            $('#updateOverlay').show();
+        }
+    })
+    .fail(function(jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error;
+        console.log("Update check failed: " + err);
+        alert("Update check failed: " + err);
+    });
+}
+
+function updateDesktopApp() {
+    console.log("updateDesktopApp");
+
+    /*
     updater.on('error', function(err, msg) {
         $('#updateOverlay').hide();
         console.log(msg);
@@ -24,6 +51,7 @@ function checkForUpdates(updater) {
     .on('update-not-available', function(err) {
         console.log("update-not-available");
     });
+    */
 
     // optional branch param can be used for testing
     // http://localhost:8080/desktop/<branch>/?platform=' + determineOperatingSystem() + '&version=' + app.getVersion();
@@ -31,6 +59,6 @@ function checkForUpdates(updater) {
     var feedUrl = 'http://update.api.phonegap.com/desktop/?platform=' + determineOperatingSystem() + '&version=' + app.getVersion();
     console.log(feedUrl);
 
-    updater.setFeedURL(feedUrl);
-    updater.checkForUpdates();
+    //updater.setFeedURL(feedUrl);
+    //updater.checkForUpdates();
 }
