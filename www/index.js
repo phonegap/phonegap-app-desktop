4c2ee679c55a54d5096372bb6473410c074464d5
Jesse MacFadyen <purplecabbage@gmail.com>
2016-06-25 22:44:26 -0700

    Prevent zooming via viewport meta tag
    Former-commit-id: b4f028fd5a709e6bf95d1320d418f99c6bde4349

--- www/index.js
+++ www/index.js
@@ -1,8 +1,8 @@
-const electron = require('electron');
+const electron = require('electron')
 // Module to control application life.
 const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+const BrowserWindow = electron.BrowserWindow
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
