2ade69435f3da115028886cbe2bfc66e8a3e7ad3
Holly Schinsky <hollyschinsky@gmail.com>
2016-08-31 15:12:29 -0400

    UI fixes for spinner overlay and template list
    [#680]Fixed the styling per Joni
    [#691]Added CSS to ensure scrollbar always shown
    
    
    Former-commit-id: b962a6f28df25e675c6333f5d2ec83f74d399b8f

--- www/js/loader.js
+++ www/js/loader.js
@@ -6,13 +6,17 @@ function showLoader(shouldFade) {
     // For longer running tasks, show spinner and fade out background until complete
     if (shouldFade) {
         // Style spinner black/white and add helpful text to show what's happening. 
-        $("#loading-overlay").css({'opacity':0.8})
+        /*$("#loading-overlay").css({'background-color':'rgba(0, 0, 0, 0.85)'})*/
+        $("#loading-overlay-sidebar").addClass('dark');
+        $("#loading-overlay-content").addClass('dark');
         $(".loader").addClass('bw'); 
         $(".loaderText").html("Creating Project...");
         $(".loaderText").css({'display':'block'});       
     }
     else {
-        $("#loading-overlay").css({'opacity':1.0});
+       /* $("#loading-overlay").css({'background-color':'rgb(204, 204, 204)'})*/
+        $("#loading-overlay-sidebar").removeClass('dark');
+        $("#loading-overlay-content").removeClass('dark');
         $(".loader").removeClass('bw');   
         $(".loaderText").css({'display':'none'});
     }
